956652c6b17bae1d9c875c406beaea867d386945
Dylan <Dylinger2002@gmail.com>
2022-03-19 13:23:48 -0400

    added fragments~ changed color of fragments~ added (fr) srtings.xml~ added appropriate comments~ other changes to match assignment requests~

--- app/src/main/java/dylan/ashton/n01442206/a3/DylanActivity.java
+++ app/src/main/java/dylan/ashton/n01442206/a3/DylanActivity.java
@@ -3,7 +3,7 @@ package dylan.ashton.n01442206.a3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+//Dylan Ashton n01442206 RNA
 public class DylanActivity extends AppCompatActivity {
 
     @Override
---
+++ app/src/main/java/dylan/ashton/n01442206/a3/Fragment4.java
@@ -0,0 +1,42 @@
+package dylan.ashton.n01442206.a3;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+//Dylan Ashton n01442206 RNA
+public class Fragment4 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Fragment4() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_4, container, false);
+    }
+}
